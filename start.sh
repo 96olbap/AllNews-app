bbe6d8717963e53215ce8e1f47daacdca921424f
96olbap <alvinwanjala@gmail.com>
2022-05-05 11:20:25 +0300

    This commit will make minor modifications to the launch files

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-export NEWS_API_KEY = 'ce73115b5d734a26bd178dd897f5e9fe'
-export SECRET_KEY = '36815769'
-
+`export NEWS_API_KEY = 'ce73115b5d734a26bd178dd897f5e9fe'`
+`export SECRET_KEY = '36815769'`
 python3.10 run.py server
